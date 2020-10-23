f553192471a150e0b88e25340aa7c52dd67fd5fb
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-10-23 13:59:04 +0100

    room name test passes

--- tests/rooms_test.py
+++ tests/rooms_test.py
@@ -8,3 +8,6 @@ class TestRoom(unittest.TestCase):
         self.room2 = Room("Scream If You Want More", "Metal", 8)
         self.room3 = Room("Timewarp", "Pre 2000", 8)
         self.room4 = Room("Get Lowww", "RnB/Hip-Hop", 8)
+
+    def test_rooms_have_names(self):
+        self.assertEqual("Timewarp", self.room3.name)
