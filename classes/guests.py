e5436e9eb75d47604ca0a0e3a863adb3ecdd0fdc
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-10-23 13:46:07 +0100

    Setup, Room, Song and Guest Classes

---
+++ classes/guests.py
@@ -0,0 +1,5 @@
+class Guest:
+    def __init__(self, name, age, preferred_genre):
+        self.name = name
+        self.age = age
+        self.preferred_genre = preferred_genre
---
+++ classes/rooms.py
@@ -0,0 +1,5 @@
+class Room:
+    def __init__(self, name, genre, capacity):
+        self.name = name
+        self.genre = genre
+        self.capacity = capacity
