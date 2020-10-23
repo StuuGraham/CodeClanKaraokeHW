b2cf7cb9cd680f6416ca36b3bc2ff684df95ca46
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-10-23 13:56:00 +0100

    setup the rooms_test for testing

---
+++ tests/rooms_test.py
@@ -0,0 +1,10 @@
+import unittest
+from classes.rooms import Room
+
+class TestRoom(unittest.TestCase):
+
+    def setUp(self):
+        self.room1 = Room("POPpin Bottles", "Pop", 8)
+        self.room2 = Room("Scream If You Want More", "Metal", 8)
+        self.room3 = Room("Timewarp", "Pre 2000", 8)
+        self.room4 = Room("Get Lowww", "RnB/Hip-Hop", 8)
